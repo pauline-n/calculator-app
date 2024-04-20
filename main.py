e7687c88a43e8c204391e78bfc7e9cb86ce772e6
Pauline Nalwanga <paulinon141@gmail.com>
2024-04-20 12:08:27 +0300

    added a simple mainapp for the calculator

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class MainApp(App):
+    pass
+
+if __name__ =='__main__':
+    MainApp().run()
